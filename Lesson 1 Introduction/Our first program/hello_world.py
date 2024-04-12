a020232593ff5fc0ef2f8953c2271492c4b4dc22
Stanislav Seltser <stanislav.seltser@petacube.com>
2024-04-12 09:33:15 -0400

    modified hello world according to chat gpt advice

--- Lesson 1 Introduction/Our first program/hello_world.py
+++ Lesson 1 Introduction/Our first program/hello_world.py
@@ -1,8 +1,10 @@
 print("Hello, world! My name is Liana")
 print("I enjoy watching movies")
-my_fav_movie="Bruce Lee"
-my_fav_actor="arnold schwarzenegger"
+my_fav_movie = "Bruce Lee"
+my_fav_actor = "arnold schwarzenegger"
 print("bye for now")
-pass
+y = 1
+z = 2  # Define the variable z
+y != z  # This will not raise an exception, but the result is not being used
 
 
