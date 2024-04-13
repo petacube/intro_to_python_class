c02de3c0f5f39633b7f3782eb0679dd725662b43
Stanislav Seltser <stanislav.seltser@petacube.com>
2024-04-12 22:36:03 -0400

    added init py

---
+++ Lesson 2 Variables/conftest.py
@@ -0,0 +1,19 @@
+
+# conftest.py
+
+import sys
+import os
+
+# Add Assignments directory to Python path
+folder_list=[
+"Arithmetic operators",
+"Assignments",
+"Boolean operators",
+"Comparison operators",
+"Type conversion",
+"Undefined variable",
+"Variable definition",
+"Variable types"
+]
+for f in folder_list:
+	sys.path.append(os.path.abspath(f))
