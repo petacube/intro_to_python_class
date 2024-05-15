d3ca274d7444e3995f2b14931a4c0414fbac9592
Fabrice Fadegnon <166365318+fabricefadegnon@users.noreply.github.com>
2024-05-15 14:02:27 +0200

    Solution propose by Fabrice Fadegnon_for_hw2.py

--- Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
+++ Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
@@ -1,1 +1,15 @@
+# Palindrome
+import re
+def is_palindrome(s):
+  # Remove non-alphanumeric characters and convert to lowercase
+  normalized_str = re.sub(r'[^A-Za-z0-9]', s).lower()
+   # Check if the normalized string is equal to its reverse
+  return normalized_str == normalized_str[::-1]
 
+# Test examples
+print(is_palindrome("Too bad-I hid a boot"))
+print(is_palindrome("A man, a plan, a canal, Panama!"))
+print(is_palindrome("Hello, World!"))
+ 
+
+  
