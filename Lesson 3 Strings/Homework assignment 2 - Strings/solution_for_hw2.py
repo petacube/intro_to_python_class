f71ecbcbbb083b5a990463be1e579e0aa5eef866
Fabrice Fadegnon <166365318+fabricefadegnon@users.noreply.github.com>
2024-05-15 16:01:20 +0200

    solution_for_problem#4_hw2.py

--- Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
+++ Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
@@ -1,1 +1,13 @@
-
+# Shuffle a string
+def shuffle_string(s, indices):
+   #Creat a list of the same lengh as the string, initialized with empty characters
+  shuffled = [''] * len(s)
+  for i, index in enumerate(indices):
+    shuffled[index] = s[i]
+  # Join the list into a single string and return
+  return ''.join(shuffled)
+# Test examples
+print(shuffle_string("abc", [2, 1, 0]))
+print(shuffle_string("aiohn", [3, 1, 4, 2, 0]))
+print(shuffle_string("art", [1, 0, 2]))
+ 
