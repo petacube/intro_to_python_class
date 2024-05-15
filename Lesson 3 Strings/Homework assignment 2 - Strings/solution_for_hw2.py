bbb5ff6a1fd067a17250a8405387ab7ebc4b1be4
Fabrice Fadegnon <166365318+fabricefadegnon@users.noreply.github.com>
2024-05-15 14:36:35 +0200

    solution_problem#2_hw2.py

--- Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
+++ Lesson 3 Strings/Homework assignment 2 - Strings/solution_for_hw2.py
@@ -1,1 +1,11 @@
-
+# Score of string
+def alphabetical_score(s):
+  score = 0
+  for char in s:
+    if char.isalpha():  # Check if the character is a letter
+      score += ord(char.lower()) - ord('a') + 1
+  return score
+# Test examples
+print(alphabetical_score("Hello"))
+print(alphabetical_score("abc"))
+print(alphabetical_score("Zebra!"))
