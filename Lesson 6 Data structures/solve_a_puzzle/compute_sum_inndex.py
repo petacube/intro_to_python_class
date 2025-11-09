3dc89144c15caff4ed3f25d42343137d649b8450
Stanislav Seltser <stanislav.seltser@petacube.com>
2025-11-08 19:32:15 -0500

    added 30 streamlit examples

--- Lesson 6 Data structures/solve_a_puzzle/compute_sum_inndex.py
+++
@@ -1,3 +0,0 @@
-compute_brute_force_indexes(input_arr,target)
-        self.assertTrue(sum_indx == [1,2],msg="Are your indexes correct")
-        sum_indx = compute_via_hash_indexes(input_arr,target)
